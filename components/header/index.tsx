070f13221a6959fba43332a0ddb9e8845e820552
R-Nhan <rubensiangbi@gmail.com>
2026-06-25 17:05:01 -0300

    Header do Sistemar pronto

---
+++ components/header/index.tsx
@@ -0,0 +1,14 @@
+import "@/components/header/header.css";
+export function Header() {
+    return (
+        <header className="header">
+            <div className="brand">
+                
+                <h1>Educa<span>Drones</span></h1>
+
+                <img src="imagens/logo.png" alt="Educa Drones" />
+                    
+            </div>
+        </header>
+    )
+}
